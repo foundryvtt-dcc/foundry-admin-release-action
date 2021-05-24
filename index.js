c992b83a2c043292680fbe9224e2de958fba4d95
Tim White <twhite@fool.com>
2021-05-23 20:45:25 -0600

    Adding release grabber info.

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const core = require('@actions/core')
+const download = require('download')
+const fs = require('fs')
 const github = require('@actions/github')
 const puppeteer = require('puppeteer')
 
+const actionToken = core.getInput('actionToken')
 const foundryAdminUsername = core.getInput('foundryAdminUsername')
 const foundryAdminPassword = core.getInput('foundryAdminPassword')
 const foundryAdminPackageURL = core.getInput('foundryAdminPackageURL')
+const manifestFileName = core.getInput('manifestFileName')
+const octokit = github.getOctokit(actionToken)
+const owner = github.context.payload.repository.owner.login
+const repo = github.context.payload.repository.name
 
-async function run () {
+async function getReleaseInfo () {
+  return await octokit.rest.repos.getLatestRelease({
+    owner: owner,
+    repo: repo,
+  })
+}
+
+async function updateFoundryAdmin (manifestURL, notesURL, compatVersion, minVersion) {
   // Initiate the Puppeteer browser
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
@@ -27,12 +41,29 @@ async function run () {
 
   // Add new package version and save
   await page.type('table tr:nth-last-child(3) .field-version > input', '0.28')
-  await page.type('table tr:nth-last-child(3) .field-manifest > input', 'https://github.com/foundryvtt-dcc/dcc/releases/download/v0.28.0/system.json')
-  await page.type('table tr:nth-last-child(3) .field-notes > input', 'https://github.com/foundryvtt-dcc/dcc/releases/tag/v0.28.0')
-  await page.type('table tr:nth-last-child(3) .field-required_core_version > input', '0.7.9')
-  await page.type('table tr:nth-last-child(3) .field-compatible_core_version > input', '0.7.9')
+  await page.type('table tr:nth-last-child(3) .field-manifest > input', manifestURL)
+  await page.type('table tr:nth-last-child(3) .field-notes > input', notesURL)
+  await page.type('table tr:nth-last-child(3) .field-required_core_version > input', minVersion)
+  await page.type('table tr:nth-last-child(3) .field-compatible_core_version > input', compatVersion)
   await page.click('#package_form > div > div.submit-row > input.default')
 
   // Close Puppeteer browser
   await browser.close()
+}
+
+async function run () {
+  // Get values for release
+  const latestRelease = await getReleaseInfo()
+  console.log(latestRelease)
+  const manifestURL = `https://github.com/${owner}/${repo}/releases/download/${latestRelease.data.tag_name}/${manifestFileName}`
+  await download(manifestURL, `./${manifestFileName}`)
+  const manifestContent = fs.readFileSync(`./${manifestFileName}`)
+  const manifest = JSON.parse(manifestContent.toString())
+  console.log(manifest.minimumCoreVersion)
+  console.log(manifest.compatibleCoreVersion)
+  console.log(manifest.manifest)
+
+  // await updateFoundryAdmin(manifestURL, notesURL, compatVersion, minVersion)
+
+
 }
